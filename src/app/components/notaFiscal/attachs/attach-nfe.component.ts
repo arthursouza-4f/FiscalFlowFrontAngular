381fc99fb86920a619214726ce2609708b54b5cb
Arthur Souza <arthursouza@MacBook-Air-de-Arthur.local>
2025-12-18 17:00:35 -0300

    fix the progress bar to fiil with the progress change

--- src/app/components/notaFiscal/attachs/attach-nfe.component.ts
+++ src/app/components/notaFiscal/attachs/attach-nfe.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-attach-nfe',
@@ -8,8 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AttachNfeComponent {
 
+  files = signal<File[]>([]);
+  percentage = signal<number>(0);
+
+  onClickIntegrar() {
+    this.percentage.set(0);
+    const interval = setInterval(() => {
+      this.percentage.update(value => {
+        if (value >= 100) {
+          clearInterval(interval);
+          return 100;
+        }
+        return value + 10;
+      });
+    }, 500);
+  }
+
+  onRemoveFile(index: number) {
+    const currentFiles = this.files();
+    currentFiles.splice(index, 1);
+  }
+
   onClickFileInput() {
     document.getElementById('fileInput')?.click();
   }
 
+  onChangeFileInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files) {
+      const fileArray = Array.from(input.files);
+      this.files.set(fileArray);
+    }
+  }
 }
